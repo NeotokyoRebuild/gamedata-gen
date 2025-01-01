f0a41e8ba5df7b0fa4447cb94cd02ad8a452243a
Masterkatze <masterkatze@gmail.com>
2025-01-01 11:40:30 +0300

    Use mmap for input file reading

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@
 
 #define R_386_32 1
 
-#include <cxxabi.h>
-
+#include <cassert>
 #include <cstdio>
 #include <cstring>
 #include <format>
@@ -18,6 +17,12 @@
 #include <memory>
 #include <string>
 #include <vector>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <cxxabi.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 struct LargeNumber
 {
@@ -98,7 +103,7 @@ struct ProgramInfo
     std::vector<RelocationInfo> relocations;
 };
 
-ProgramInfo process(const std::string& image)
+ProgramInfo process(char *image, std::size_t size)
 {
     ProgramInfo programInfo = {};
 
@@ -108,7 +113,7 @@ ProgramInfo process(const std::string& image)
         return programInfo;
     }
 
-    Elf *elf = elf_memory(const_cast<char *>(image.data()), image.size());
+    Elf *elf = elf_memory(image, size);
     if (!elf)
     {
         programInfo.error = "elf_begin failed. (" + std::string(elf_errmsg(-1)) + ")";
@@ -357,10 +362,10 @@ auto demangleSymbol(const char *abiName)
     return retval;
 }
 
-std::vector<unsigned char> getDataForSymbol(const ProgramInfo &programInfo, const SymbolInfo &symbol)
+std::span<const unsigned char> getDataForSymbol(const ProgramInfo &programInfo, const SymbolInfo &symbol)
 {
     LargeNumber dataStart;
-    std::vector<RodataChunk> dataChunks;
+    const std::vector<RodataChunk> *dataChunks;
 
     if (symbol.section == 0)
     {
@@ -369,12 +374,12 @@ std::vector<unsigned char> getDataForSymbol(const ProgramInfo &programInfo, cons
     else if (symbol.section == programInfo.rodataIndex)
     {
         dataStart = programInfo.rodataStart;
-        dataChunks = programInfo.rodataChunks;
+        dataChunks = &programInfo.rodataChunks;
     }
     else if (symbol.section == programInfo.relRodataIndex)
     {
         dataStart = programInfo.relRodataStart;
-        dataChunks = programInfo.relRodataChunks;
+        dataChunks = &programInfo.relRodataChunks;
     }
     else
     {
@@ -386,17 +391,17 @@ std::vector<unsigned char> getDataForSymbol(const ProgramInfo &programInfo, cons
         throw ">= 32-bit rodata is not supported";
     }
 
-    for (const auto& chunk : dataChunks)
+    for (const auto& chunk : *dataChunks)
     {
         if (chunk.offset.high != 0)
         {
             throw ">= 32-bit rodata is not supported";
         }
 
-        auto start = symbol.address.low - (dataStart.low + chunk.offset.low);
-        auto end = start + symbol.size.low;
+        int start = symbol.address.low - (dataStart.low + chunk.offset.low);
+        int end = start + symbol.size.low;
 
-        if (start < 0 || end > chunk.data.size())
+        if (start < 0 || end > static_cast<int>(chunk.data.size()))
         {
             continue;
         }
@@ -497,7 +502,7 @@ Out parse(ProgramInfo &programInfo)
 
         using DataViewType = std::uint32_t;
         const int BYTES_PER_ELEMENT = sizeof(DataViewType); // 4 from Uint32Array.BYTES_PER_ELEMENT
-        auto symbolDataView = std::span(reinterpret_cast<DataViewType*>(symbolData.data()), symbolData.size() / BYTES_PER_ELEMENT);
+        auto symbolDataView = std::span(reinterpret_cast<const DataViewType*>(symbolData.data()), symbolData.size() / BYTES_PER_ELEMENT);
 
         VTable *classVTable;
 
@@ -675,7 +680,7 @@ std::vector<Out2> formatVTable(const ClassInfo &classInfo)
     auto vtableInfo = classInfo.vtables.at(vtableIndex);
 
     int windowsIndex = 0;
-    for (int linuxIndex = 0; linuxIndex < vtableInfo.functions.size(); ++linuxIndex)
+    for (int linuxIndex = 0; linuxIndex < static_cast<int>(vtableInfo.functions.size()); ++linuxIndex)
     {
         auto functionInfo = vtableInfo.functions.at(linuxIndex);
 
@@ -714,7 +719,7 @@ std::vector<Out2> formatVTable(const ClassInfo &classInfo)
                     previousOverloads++;
                 }
 
-                while ((linuxIndex + 1 + remainingOverloads) < vtableInfo.functions.size())
+                while ((linuxIndex + 1 + remainingOverloads) < static_cast<int>(vtableInfo.functions.size()))
                 {
                     auto nextFunctionIndex = linuxIndex + 1 + remainingOverloads;
                     auto nextFunctionInfo = vtableInfo.functions.at(nextFunctionIndex);
@@ -746,7 +751,101 @@ std::vector<Out2> formatVTable(const ClassInfo &classInfo)
     }
 
     return vtable;
-  }
+}
+
+class mmapReader
+{
+public:
+    explicit mmapReader(const std::string& path)
+    {
+        auto fd = ::open(path.c_str(), O_RDONLY);
+        if(fd == -1)
+        {
+            throw std::runtime_error(std::format("Failed to open file \"{}\": {} (errno={}) ", path, strerror(errno), errno));
+        }
+
+        struct stat sb {};
+        if(fstat(fd, &sb) == -1)
+        {
+            throw std::runtime_error(std::format("stat failed for file \"{}\": {} (errno={}) ", path, strerror(errno), errno));
+        }
+
+        auto file_size = static_cast<std::size_t>(sb.st_size);
+
+        auto page_size = static_cast<std::size_t>(sysconf(_SC_PAGESIZE));
+        std::size_t remainder = file_size % page_size;
+
+        auto mem_size = file_size;
+        if(remainder != 0)
+        {
+            mem_size += page_size - remainder;
+        }
+
+        auto data = mmap(nullptr, mem_size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+        if(file_size != 0)
+        {
+            if(data == MAP_FAILED)
+            {
+                throw std::runtime_error(std::format("mmap failed for file \"{}\": {} (errno={}) ", path, strerror(errno), errno));
+            }
+        }
+
+        m_data = static_cast<char*>(data);
+        m_fd = fd;
+        m_file_size = file_size;
+        m_mem_size = mem_size;
+    }
+
+    ~mmapReader()
+    {
+        assert(m_fd != -1);
+        assert(m_data);
+
+        if(m_mem_size != 0)
+        {
+            auto res = madvise(reinterpret_cast<void*>(m_data), m_mem_size, MADV_DONTNEED | MADV_FREE);
+            if(res == -1)
+            {
+                std::cout << std::format("madvise failed: {} (errno={}) ", strerror(errno), errno);
+            }
+
+            res = munmap(reinterpret_cast<void*>(m_data), m_mem_size);
+            if(res != 0)
+            {
+                std::cout << std::format("munmap failed with result {}: {} (errno={}) ", res, strerror(errno), errno);
+            }
+        }
+
+        auto res = posix_fadvise(m_fd, 0, static_cast<off_t>(m_file_size), POSIX_FADV_DONTNEED); //POSIX_FADV_NOREUSE
+        if(res != 0)
+        {
+            std::cout << std::format("posix_fadvise failed: {} (errno={}) ", strerror(errno), errno);
+        }
+
+        res = ::close(m_fd);
+        if(res == -1)
+        {
+            std::cout << std::format("Failed to close file descriptor {}: {} (errno={}) ", m_fd, strerror(errno), errno);
+        }
+    }
+
+    char *data()
+    {
+        return m_data;
+    }
+
+    std::size_t size()
+    {
+        return m_file_size;
+    }
+
+private:
+    int m_fd{-1};
+    char *m_data{};
+    std::size_t m_file_size{0};
+    std::size_t m_mem_size{0};
+};
 
 int main(int argc, char *argv[])
 {
@@ -756,10 +855,18 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+#if 0
     std::ifstream file(argv[1], std::ios::binary);
     std::string image((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    auto program = image.data();
+    auto size = image.size();
+#else
+    mmapReader reader(argv[1]);
+    auto program = reader.data();
+    auto size = reader.size();
+#endif
 
-    ProgramInfo programInfo = process(image);
+    ProgramInfo programInfo = process(program, size);
 
     if (!programInfo.error.empty())
     {
